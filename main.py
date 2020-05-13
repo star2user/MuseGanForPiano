7cbd4fd93ca0262b75bef8a5bb54b6978f6c4052
star2usemap <star2usemap@gmail.com>
2020-05-13 17:01:09 +0900

    Train시 배치사이즈, 마디 개수, 트랙 수 수정, Hanon으로 훈련

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ import types
 from music21 import midi
 from music21 import note, stream, duration
 
-SECTION = 'compose'
-RUN_ID = '0017'
-DATA_NAME = 'chorales'
-
 RUN_FOLDER = 'midi/'
 
 if not os.path.exists(RUN_FOLDER):
@@ -20,14 +16,14 @@ if not os.path.exists(RUN_FOLDER):
     os.mkdir(os.path.join(RUN_FOLDER, 'weights'))
     os.mkdir(os.path.join(RUN_FOLDER, 'samples'))
 #load data
-BATCH_SIZE = 32
-n_bars = 2
+BATCH_SIZE = 12
+n_bars = 4
 n_steps_per_bar = 96
 n_pitches = 84
-n_tracks = 2
+n_tracks = 1
 z_dim = 32
 
-x_train = np.load('easyScore2.npy')
+x_train = np.load('Hanon.npy')
 print(x_train.shape)
 x_train = x_train.reshape(-1, n_bars, n_steps_per_bar, n_pitches, n_tracks)
 print(x_train.shape)
@@ -50,14 +46,10 @@ gan.barGen[0].summary()
 gan.generator.summary()
 gan.critic.summary()
 
-EPOCHS = 2000
-PRINT_EVERY_N_BATCHES = 20
+EPOCHS = 1000
+PRINT_EVERY_N_BATCHES = 100
 
 '''
-gan.epoch = 0
-
-'''
-
 gan.load_weights(RUN_FOLDER)
 gan.epoch = 2000;
 '''
@@ -68,40 +60,18 @@ gan.train(
     , run_folder = RUN_FOLDER
     , print_every_n_batches = PRINT_EVERY_N_BATCHES
 )
-'''
-r = 5
+
+r = 2
 chords_noise = np.random.normal(0, 1, (r, z_dim))
 style_noise = np.random.normal(0, 1, (r, z_dim))
 melody_noise = np.random.normal(0, 1, (r, n_tracks, z_dim))
 groove_noise = np.random.normal(0, 1, (r, n_tracks, z_dim))
 score = gan.generator.predict([chords_noise, style_noise, melody_noise, groove_noise])
 
-'''
-def TrimScore(score, leastNoteBeat):
-    # score: (batchSize, 2, 96, 84 2) 형태의 배열
-    # leastNoteBeat : 악보에서 나올 수 있는 음표의 최소박자
-    output = np.array(score)
-
-    batchSize = score.shape[0]
-    count = score.shape[2] // leastNoteBeat;
-    barCount = score.shape[1]
-    pitchCount = score.shape[3]
-    trackCount = score.shape[4]
-
-    for dataNumber in range(batchSize):
-      for trackNumber in range (trackCount):
-          for barNumber in range (barCount):
-             for i in range (leastNoteBeat):
-                  for pitchNumber in range (pitchCount):
-                      output[dataNumber, barNumber, i*count:(i+1)*count, pitchNumber, trackNumber] = score[dataNumber, barNumber, i*count, pitchNumber, trackNumber]
-
-    return output
- '''
 gan.notes_to_midi(RUN_FOLDER, score, 'Originalsample')
 origninalScore = score
 
 score = gan.TrimScore(origninalScore, 16)
-
-gan.notes_to_midi(RUN_FOLDER, score, 'Trim16sample0')
+gan.notes_to_midi(RUN_FOLDER, score, 'Trim16sample')
 
 
