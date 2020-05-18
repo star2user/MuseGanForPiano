29b52203e120cbb046aaf7ba005bc9deef9c0d36
star2usemap <star2usemap@gmail.com>
2020-05-18 13:37:53 +0900

    no message

--- VerivicateNpy.py
+++ VerivicateNpy.py
@@ -3,20 +3,19 @@ from pypianoroll import Multitrack, Track
 from src.MuseGAN import MuseGAN
 
 BATCH_SIZE = 32
-n_bars = 2
+n_bars = 4
 n_steps_per_bar = 96
 n_pitches = 84
 n_tracks = 1
 z_dim = 32
 
-x_train = np.load('Hanon.npy')
+x_train = np.load('Train4,4.npy')
 #zeros = np.zeros_like(x_train)
 
 #x_train = np.concatenate((x_train, zeros), axis=4)
 
 print(x_train.shape)
 x_train = x_train.reshape(-1, n_bars, n_steps_per_bar, n_pitches, n_tracks)
-x_train = x_train[0:10]
 
 gan = MuseGAN(input_dim = (n_bars, n_steps_per_bar, n_pitches, n_tracks)
         , critic_learning_rate = 0.001
@@ -31,4 +30,7 @@ gan = MuseGAN(input_dim = (n_bars, n_steps_per_bar, n_pitches, n_tracks)
         , n_pitches = n_pitches
         )
 
-gan.notes_to_midi(run_folder='midi', output=x_train, filename="HanonMidi")
+length = len(x_train)
+
+gan.notes_to_midi(run_folder='midi', output=x_train[0:10], filename="TestMidi")
+gan.notes_to_midi(run_folder='midi', output=x_train[length-10:length-1], filename="TestMidi2")
--- main.py
+++ main.py
@@ -16,14 +16,14 @@ if not os.path.exists(RUN_FOLDER):
     os.mkdir(os.path.join(RUN_FOLDER, 'weights'))
     os.mkdir(os.path.join(RUN_FOLDER, 'samples'))
 #load data
-BATCH_SIZE = 12
+BATCH_SIZE = 32
 n_bars = 4
 n_steps_per_bar = 96
 n_pitches = 84
 n_tracks = 1
 z_dim = 32
 
-x_train = np.load('Hanon.npy')
+x_train = np.load('Train4,4.npy')
 print(x_train.shape)
 x_train = x_train.reshape(-1, n_bars, n_steps_per_bar, n_pitches, n_tracks)
 print(x_train.shape)
@@ -42,43 +42,10 @@ gan = MuseGAN(input_dim = x_train.shape[1:]
         , n_pitches = n_pitches
         )
 
-EPOCHS = 2000
+
+EPOCHS = 4000
 PRINT_EVERY_N_BATCHES = 100
 
-gan.load_weights(RUN_FOLDER)
-
-r = 2
-chords_noise = np.random.normal(0, 1, (r, z_dim))
-style_noise = np.random.normal(0, 1, (r, z_dim))
-melody_noise = np.random.normal(0, 1, (r, n_tracks, z_dim))
-groove_noise = np.random.normal(0, 1, (r, n_tracks, z_dim))
-score = gan.generator.predict([chords_noise, style_noise, melody_noise, groove_noise])
-
-origninalScore = score
-gan.notes_to_midi(RUN_FOLDER, origninalScore, 'Original')
-gan.notes_to_midi(RUN_FOLDER, gan.TrimScore(origninalScore, 16), 'Hanon_2000train_1')
-
-r = 5
-chords_noise = np.random.normal(0, 1, (r, z_dim))
-style_noise = np.random.normal(0, 1, (r, z_dim))
-melody_noise = np.random.normal(0, 1, (r, n_tracks, z_dim))
-groove_noise = np.random.normal(0, 1, (r, n_tracks, z_dim))
-score = gan.generator.predict([chords_noise, style_noise, melody_noise, groove_noise])
-
-origninalScore = score
-gan.notes_to_midi(RUN_FOLDER, gan.TrimScore(origninalScore, 16), 'Hanon_2000train_2')
-
-chords_noise = chords_noise * 2
-style_noise = style_noise
-melody_noise = melody_noise
-groove_noise = groove_noise
-score = gan.generator.predict([chords_noise, style_noise, melody_noise, groove_noise])
-
-origninalScore = score
-gan.notes_to_midi(RUN_FOLDER, gan.TrimScore(origninalScore, 16), 'Hanon_2000train_3')
-#gan.epoch = 1000;
-
-'''
 gan.train(
     x_train
     , batch_size = BATCH_SIZE
@@ -86,8 +53,5 @@ gan.train(
     , run_folder = RUN_FOLDER
     , print_every_n_batches = PRINT_EVERY_N_BATCHES
 )
-'''
-
-
 
 
